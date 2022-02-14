5dd2743d62ba82d5e63b6210b4893592677def80
Robson Leal Notebook <devleal@hotmail.com>
2022-02-14 16:25:52 -0300

    Finalizando a primeira versao do app de enquetes

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,11 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        """
+        Questão foi publicada recentemente?
+        """
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
