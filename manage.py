ba5e70ad1ba05987c470760e213c5dbd2927f697
Robson Leal Notebook <devleal@hotmail.com>
2022-02-14 13:31:52 -0300

    criando templates e rotas da aplicaçao

--- manage.py
+++ manage.py
@@ -11,9 +11,8 @@ def main():
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Não foi possível importar o Django, tem certeza de que ele está instalado e "
+            "disponível em sua PYTHONPATH? Você lembrou de ativar seu ambiente virtual? "
         ) from exc
     execute_from_command_line(sys.argv)
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def index_page(request):
+    return HttpResponse('Hello World! Here is the index!')
 
 urlpatterns = [
+    path('', index_page),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
